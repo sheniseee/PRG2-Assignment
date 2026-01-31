25258c17d2e780c858fce114b14f8f037bfa432c
sheniseee <s10273890@connect.np.edu.sg>
2026-02-01 02:25:06 +0800

    1, 4, 6, 8 Features

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using PRG2-Assignment;
 
 class Program
 {
@@ -10,16 +11,141 @@ class Program
         List<Customer> customers = new List<Customer>();
         List<Order> orders = new List<Order>();
 
+        while (true)
+        {
+            Console.WriteLine("===== Gruberoo Food Delivery System ===== ");
+            Console.WriteLine("1.List all restaurants and menu items"):
+            Console.WriteLine("2.List all orders");
+            Console.WriteLine("3.Create a new order");
+            Console.WriteLine("4.Process an order");
+            Console.WriteLine("5.Modify an existing order");
+            Console.WriteLine("6.Delete an existing order");
+            Console.WriteLine("0.Exit");
+
+            Console.Write("Enter your choice: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 0)
+            {
+                break;
+            }
+            else if (choice == 1)
+            {
+                Feature3_ListAllRestaurantsAndMenuItems();
+            }
+            else if (choice == 2)
+            {
+                Feature4_ListAllOrders();
+            }
+            else if (choice == 3)
+            {
+                Feature5_CreateNewOrder();
+            }
+            else if (choice == 4)
+            {
+                Feature6_ProcessOrder();
+            }
+            else if (choice == 5)
+            {
+                ModifyExistingOrder();
+            }
+            else if (choice == 6)
+            {
+                Feature8_DeleteExistingOrder();
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please try again.");
+            }
+
+        }
+
+        // Calls for feature 1 (Loading of restaurants and food items files)
+        LoadRestaurantsAndFoodItems();
+
         // Calls for feature 2 (Loading of customers and orders files)
         LoadCustomersAndOrders(customers, orders);
 
-        // Calls for feature 3
-
-        // Calls for feature 5
-
-        // Calls for feature 7 
 
     }
+    //========================================================== =
+    // Student Number : S10273890E
+    // Student Name : Shenise Lim Em Qing
+    // Partner Name : Chloe Heng Chi Xuan
+    //========================================================== =
+    // Feature 1
+    static void LoadRestaurantsAndFoodItems()
+    {
+        //restaurants
+        string restaurantsFile = "restaurants.csv";
+
+        using (StreamReader sr = new StreamReader(restaurantsFile))
+        {
+            string header = sr.ReadLine();
+
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] parts = line.Split(',');
+
+                if (parts.Length >= 3)
+                {
+                    string restaurantID = parts[0];
+                    string restaurantName = parts[1];
+                    string restaurantEmail = parts[2];
+
+                    Restaurant restaurant = new Restaurant(restaurantID, restaurantName, restaurantEmail);
+                    restaurants.Add(restaurant);
+                }
+            }
+        }
+
+        //food items
+        string foodItemsFile = "fooditems.csv";
+
+        using (StreamReader sr = new StreamReader(foodItemsFile))
+        {
+            string header = sr.ReadLine();
+
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] parts = line.Split(',');
+                if (parts.Length >= 4)
+                {
+                    string restaurantId = parts[0];
+                    string itemName = parts[1];
+                    string itemDesc = parts[2];
+                    double itemPrice = Convert.ToDouble(parts[3]);
+
+                    FoodItem food = new FoodItem(itemName, itemDesc, itemPrice, "");
+
+                    Restaurant r = null;
+
+                    foreach (Restaurant rest in restaurants)
+                    {
+                        if (rest.RestaurantId == restaurantId)
+                        {
+                            r = rest;
+                            break;
+                        }
+                    }
+
+                    if (r != null)
+                    {
+                        if (r.Menus.Count == 0)
+                        {
+                            r.Menus.Add(new Menu("M1", "Main Menu"));
+                        }
+
+                        r.Menus[0].FoodItems.Add(food);
+                    }
+                }
+            }
+        }
+    }
+
+
 
     //========================================================== =
     // Student Number : S10272203K
@@ -100,6 +226,49 @@ class Program
             }
         }
     }
+
+
+    //========================================================== =
+    // Student Number : S10273890E
+    // Student Name : Shenise Lim Em Qing
+    // Partner Name : Chloe Heng Chi Xuan
+    //========================================================== =
+    // Feature 4
+    static void Feature4_ListAllOrders()
+    {
+        Console.WriteLine("All Orders ");
+        Console.WriteLine("===========");
+        Console.WriteLine("Order ID   Customer      Restaurant       Delivery Date/Time   Amount    Status  ");
+        Console.WriteLine("--------   ----------    -------------    ------------------   ------    --------- ");
+
+        foreach (Restaurant r in restaurants)
+        {
+            foreach (Order order in r.OrderQueue)
+            {
+                string customerName = "Unknown";
+
+                foreach (Customer c in customers)
+                {
+                    if (c.Orders.Contains(order))
+                    {
+                        customerName = c.CustomerName;
+                        break;
+                    }
+                }
+
+                Console.WriteLine(
+                    $"{order.OrderID,-10} " +
+                    $"{r.RestaurantName,-15} " +
+                    $"{order.DeliveryDateTime:dd/MM/yyyy HH:mm}   " +
+                    $"{order.OrderTotal,8:C}   " +
+                    $"{order.OrderStatus,-10}"
+                );
+            }
+        }
+    }
+
+
+
 
     //========================================================== =
     // Student Number : S10272203K
@@ -339,12 +508,137 @@ class Program
     }
 
     //========================================================== =
+    // Student Number : S10273890E
+    // Student Name : Shenise Lim Em Qing
+    // Partner Name : Chloe Heng Chi Xuan
+    //========================================================== =
+    // Feature 6
+
+    static void Feature6_ProcessOrder()
+    {
+        Console.WriteLine("Process Order");
+        Console.WriteLine("=============");
+
+        Console.Write("Enter Restaurant ID: ");
+        string restaurantID = Console.ReadLine();
+
+        Restaurant selectedRestaurant = null;
+        foreach (Restaurant r in restaurants)
+        {
+            if (r.RestaurantId == restaurantID)
+            {
+                selectedRestaurant = r;
+                break;
+            }
+        }
+
+        if (selectedRestaurant == null)
+        {
+            Console.WriteLine("Invalid Restaurant ID.");
+            return;
+        }
+
+        if (selectedRestaurant.OrderQueue.Count == 0)
+        {
+            Console.WriteLine("No orders available for this restaurant.");
+            return;
+        }
+
+        int queueCount = selectedRestaurant.OrderQueue.Count;
+
+        for (int i = 0; i < queueCount; i++)
+        {
+            Order order = selectedRestaurant.OrderQueue.Dequeue();
+
+            Console.WriteLine($"Order {order.OrderID}:");
+            Console.WriteLine($"Customer: {customerName}");
+
+            Console.WriteLine("Ordered Items: ");
+            for (int j = 0; j < order.OrderedFoodItems.Count; j++)
+            {
+                OrderedFoodItem item = order.OrderedFoodItems[j];
+                Console.WriteLine($"{j + 1}. {item.FoodItem.ItemName} - {item.QtyOrdered}");
+            }
+
+            Console.WriteLine($"Delivery date/time: {order.DeliveryDateTime:dd/MM/yyyy HH:mm}");
+            Console.WriteLine($"Total Amount: ${order.OrderTotal:C2}");
+            Console.WriteLine($"Order Status: {order.OrderStatus}");
+
+
+
+            Console.Write("[C]onfirm / [R]eject / [S]kip / [D]eliver: ");
+            string action = Console.ReadLine().ToUpper();
+
+            if (action == "C")
+            {
+                if (order.OrderStatus == "Pending")
+                {
+                    order.OrderStatus = "Preparing";
+                    Console.WriteLine($"Order {order.OrderID} confirmed. Status: Preparing");
+                }
+                else
+                {
+                    Console.WriteLine("Cannot confirm. Order must be Pending.");
+                }
+            }
+            else if (action == "R")
+            {
+                if (order.OrderStatus == "Pending")
+                {
+                    order.OrderStatus = "Rejected";
+                    refundStack.Push(order);
+
+                    Console.WriteLine($"Order {order.OrderID} rejected.");
+                    Console.WriteLine($"Refund of ${order.OrderTotal:C2} processed.");
+                }
+                else
+                {
+                    Console.WriteLine("Cannot reject. Order must be Pending.");
+                }
+            }
+            else if (action == "D")
+            {
+                if (order.OrderStatus == "Preparing")
+                {
+                    order.OrderStatus = "Delivered";
+                    Console.WriteLine($"Order {order.OrderID} delivered successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Cannot deliver. Order must be Preparing.");
+                }
+            }
+            else if (action == "S")
+            {
+                if (order.OrderStatus == "Cancelled")
+                {
+                    Console.WriteLine($"Order {order.OrderID} is cancelled. Skipping...");
+                    selectedRestaurant.OrderQueue.Enqueue(order);
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Cannot skip. Order must be Cancelled.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid action. Order skipped.");
+                selectedRestaurant.OrderQueue.Enqueue(order);
+            }
+
+        }
+    }
+
+
+
+    //========================================================== =
     // Student Number : S10272203K
     // Student Name : Chloe Heng Chi Xuan
     // Partner Name : Shenise Lim Em Qing 
     //========================================================== =
     // Feature 7
-    static void ModifyExistingOrder(List<Customer> customers,List<Restaurant> restaurants,List<Order> orders)
+    static void ModifyExistingOrder(List<Customer> customers, List<Restaurant> restaurants, List<Order> orders)
     {
         Console.WriteLine("\nModify Existing Order");
         Console.WriteLine("=========================");
@@ -463,6 +757,107 @@ class Program
 
         Console.WriteLine($"Order {target.OrderID} updated.");
         Console.WriteLine($"New Delivery Date/Time: {target.DeliveryDateTime:dd/MM/yyyy HH:mm}");
-    
+
     }
-         
+
+    //========================================================== =
+    // Student Number : S10273890E
+    // Student Name : Shenise Lim Em Qing
+    // Partner Name : Chloe Heng Chi Xuan
+    //========================================================== =
+    // Feature 8
+
+    static void Feature8_DeleteExistingOrder()
+    {
+        Console.WriteLine("Delete Order ");
+        Console.WriteLine("=============");
+
+        Console.Write("Enter Customer Email: ");
+        string customerEmail = Console.ReadLine();
+
+        Customer selectedCustomer = null;
+        foreach (Customer c in customers)
+        {
+            if (c.EmailAddress == customerEmail)
+            {
+                selectedCustomer = c;
+                break;
+            }
+        }
+
+        if (selectedCustomer == null)
+        {
+            Console.WriteLine("Invalid customer email.");
+            return;
+        }
+
+
+        List<Order> pendingOrders = new List<Order>();
+        foreach (Order o in selectedCustomer.Orders)
+        {
+            if (o.OrderStatus == "Pending")
+            {
+                pendingOrders.Add(o);
+            }
+        }
+
+        if (pendingOrders.Count == 0)
+        {
+            Console.WriteLine("No Pending orders found.");
+            return;
+        }
+
+        Console.WriteLine("Pending Orders:");
+        foreach (Order o in pendingOrders)
+        {
+            Console.WriteLine(o.OrderID);
+        }
+
+        Console.Write("Enter Order ID: ");
+        int orderID = Convert.ToInt32(Console.ReadLine());
+
+        Order targetOrder = null;
+        foreach (Order o in pendingOrders)
+        {
+            if (o.OrderID == orderID)
+            {
+                targetOrder = o;
+                break;
+            }
+        }
+
+        if (targetOrder == null)
+        {
+            Console.WriteLine("Invalid Order ID.");
+            return;
+        }
+
+        Console.WriteLine($"Customer: {selectedCustomer.CustomerName}");
+
+        Console.WriteLine("Ordered Items:");
+        for (int i = 0; i < targetOrder.OrderedFoodItems.Count; i++)
+        {
+            OrderedFoodItem item = targetOrder.OrderedFoodItems[i];
+            Console.WriteLine($"{i + 1}. {item.FoodItem.ItemName} - {item.QtyOrdered}");
+        }
+
+        Console.WriteLine($"Delivery date/time: {targetOrder.DeliveryDateTime:dd/MM/yyyy HH:mm}");
+        Console.WriteLine($"Total Amount: ${targetOrder.OrderTotal:C2}");
+        Console.WriteLine($"Order Status: {targetOrder.OrderStatus}");
+
+        Console.Write("Confirm deletion? [Y/N]: ");
+        string confirm = Console.ReadLine().ToUpper();
+
+        if (confirm != "Y")
+        {
+            Console.WriteLine("Deletion cancelled.");
+            return;
+        }
+
+        targetOrder.OrderStatus = "Cancelled";
+        refundStack.Push(targetOrder);
+
+        Console.WriteLine($"Order {targetOrder.OrderID} cancelled. Refund of {targetOrder.OrderTotal:C} processed.");
+
+    }
+}
